56a0e87165d6e9b69bc38cb4c370771735337d70
jamesmf <jamesmfrick@gmail.com>
2020-02-18 23:05:13 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+To submit your skill, replace this file with text from
+https://rawgit.com/MycroftAI/mycroft-skills/master/meta_editor.html
+
+
+## two-way-mycroft-adapter
+Allows two-way communication between Mycroft and Mozilla IoT
+
+## Description
+This skill allows you to speak to Mycroft from the Mozilla IoT Gateway by registering it as a `webthing` and to control the Mozilla IoT Hub via Mycroft
+
+## Examples
+* "Turn on the light"
+
+## Credits
+jamesmf
---
+++ __init__.py
@@ -0,0 +1,91 @@
+# The MIT License (MIT)
+# Copyright (c) 2019 jamesmf
+
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill, intent_handler
+from mycroft.util.log import LOG
+from __future__ import division
+from webthing import Action, Event, Property, SingleThing, Thing, Value, WebThingServer
+from mycroft_bus_client import MessageBusClient, Message
+import logging
+import time
+import uuid
+
+
+class QuestionAction(Action):
+    def __init__(self, thing, input_):
+        Action.__init__(self, uuid.uuid4().hex, thing, "question", input_=input_)
+
+    def perform_action(self):
+        print(self.input)
+        self.thing.client.emit(
+            Message("question:query", data={"phrase": self.input["query"]})
+        )
+
+
+class SpeakEvent(Event):
+    def __init__(self, thing, data):
+        Event.__init__(self, thing, "mycroftsaid", data=data)
+
+
+class MycroftAsWoTSkill(MycroftSkill):
+
+    # The constructor of the skill, which calls MycroftSkill's constructor
+    def __init__(self):
+        super(MycroftAsWoTSkill, self).__init__(name="RasaSkill")
+        self.client = MessageBusClient()
+        self.thing = make_thing()
+
+        self.run_server()
+
+    def run_server(self):
+        def print_utterance(message):
+            if message.data.get("answer") is not None:
+                print('Mycroft said "{}"'.format(message.data.get("answer")))
+                self.thing.add_event(SpeakEvent(thing, message.data.get("answer")))
+            else:
+                if "utterance" in message.data:
+                    print(message.data["utterance"], "utterance!")
+                    self.thing.add_event(
+                        SpeakEvent(thing, data=message.data["utterance"])
+                    )
+
+        self.client.on("question:query.response", print_utterance)
+        self.client.on("speak", print_utterance)
+
+        self.client.run_in_thread()
+
+        # If adding more than one thing, use MultipleThings() with a name.
+        # In the single thing case, the thing's name will be broadcast.
+        self.server = WebThingServer(SingleThing(thing), port=8888)
+        try:
+            self.server.start()
+        except KeyboardInterrupt:
+            self.server.stop()
+
+    def make_thing(self):
+        thing = Thing(
+            "test:mycroft:mycroft-hook-1234", "Mycroft", [], "A controller for Mycroft"
+        )
+        thing.add_available_action(
+            "question",
+            {
+                "title": "Question",
+                "description": "Send a query to Mycroft",
+                "input": {
+                    "type": "object",
+                    "required": ["query"],
+                    "properties": {"query": {"type": "string"}},
+                },
+            },
+            QuestionAction,
+        )
+        thing.add_available_event(
+            "mycroftsaid", {"description": "Mycroft Said:", "type": "string"}
+        )
+
+        return thing
+
+
+def create_skill():
+    return MycroftAsWoTSkill()
